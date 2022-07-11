37ff2093d19f8cfd09229c2d981c41798b57f7e2
Thiago Silva <silvaa.thiagod2@gmail.com>
2022-07-11 09:36:34 -0300

    backend complete

---
+++ src/main/java/dev/thiagod/systemtask/SystemTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.thiagod.systemtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thiagod/systemtask/SystemTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagod.systemtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
